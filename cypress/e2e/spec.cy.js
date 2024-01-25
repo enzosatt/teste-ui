c262ae4c9f99093c72a35f67aa3afcb4ceaf9942
Enzo <enzoteixeira587@gmail.com>
2024-01-25 18:21:18 -0300

    alterações parte 2

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
+
 context ('Funcionalidade Login', () => {
  beforeEach(() => {
   cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
@@ -10,11 +12,28 @@ afterEach(() => {
 
   it('Deve fazer login com sucesso', () =>{
   
-  cy.get ('#username'). type ('aluno_ebac@teste.com')
-  cy.get('#password') . type ('teste@teste.com')
+  cy.get ('#username'). type (perfil.usuario)
+  cy.get('#password') . type (perfil.senha)
   cy.get ('.woocommerce-form > .button'). click()
   cy.get('.breadcrumb > .active') . should('contain', 'Minha conta')
   cy.get('#primary-menu > .menu-item-536 > .dropdown-toggle') . should ('contain', 'Home')
+
+it('Deve fazer login com sucesso - Usando fixture', () => {
+  cy.fixture('perfil') .then (dados => {
+    cy.get ('#username'). type (dados.usuario)
+  cy.get('#password') . type (dados.senha, {log:false})
+  cy.get ('.woocommerce-form > .button'). click()
+  cy.get('.breadcrumb > .active') . should('contain', 'Minha conta')
+  })
+});
+
+  it('', () => {
+    cy.get ('#username'). type ('aluno_ebac@teste.com')
+  cy.get('#password') . type ('teste@teste.com')
+  cy.get ('.woocommerce-form > .button'). click()
+
+  cy.get('.breadcrumb > .active') . should('contain', 'Minha conta')
+  });
 
 
 
